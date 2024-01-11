36d4ea150c4e6ac145490dfeb35111a1aac66962
Lorri Lanig <137334064+lorrilanig@users.noreply.github.com>
2024-01-11 13:48:35 -0600

    debugging-complete

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -5,6 +5,7 @@
 // Is the program behaving as expected?
 
 let launchReady = false;
+let crewReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
@@ -21,13 +22,13 @@ console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("launchReady = ", crewReady);
 
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
