86295d139f3e66e9dd092b556e8316ccd3b0649d
Lorri Lanig <lorrilanig.art@gmail.com>
2024-01-15 14:36:09 -0600

    Strings-complete

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,4 +1,22 @@
 // Initialize Variables below
+let date = 'Monday 2019-03-18';
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronauntCount * averageAstronautMassKg;
+let fuelMasKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel= 100;
+let weatherStatus = 'clear';
+let preparedForLiftOff = true;
+
+
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
@@ -7,7 +25,11 @@
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
 
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
+if (fuelTempCelsius > -300 || fuelTempCelsius < -150) {
+ console.log(true);
+} else {
+    console.log(false);
+}
 // add logic below to verify the fuel level is at 100%
 
 // add logic below to verify the weather status is clear
--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,16 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+num = String(num);
+console.log(num.length);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+num = String(123.45);
+console.log(num.length);
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+//num = String(239480);
+if (num.includes('.')) {
+    console.log(num.length-1);
+} else {
+    console.log(num.length)
+};
+
