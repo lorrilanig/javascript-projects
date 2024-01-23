7b0d2bcfdef9a27c67f5226000b255773eae1134
Lorri Lanig <137334064+lorrilanig@users.noreply.github.com>
2024-01-23 15:16:20 -0600

    complete

---
+++ functions/excersies/excersies.js
@@ -0,0 +1,40 @@
+//RECTANGLES
+function makeLine(size) {
+    let line = '';
+    for (let i = 0; i < size; i++) {
+        line += '#'; 
+    }
+    return line;
+};
+//console.log(makeLine(5));
+
+//SQUARE 
+function makeSquare(size) {
+    let square = '';
+    for (let i = 0; i < size; i++){
+        square += makeLine(size) + '\n';       
+    }
+    return square;
+};
+//console.log(makeSquare(5));
+
+//WIDTH AND HEIGHT RECTANGLE
+
+function makeRectangle(width, height) {
+    rectangle = '';
+    for (let i = 0; i < height; i++) {
+        rectangle += (makeLine(width) + '\n');
+    }
+    return rectangle;
+};
+//console.log(makeRectangle(5,3));
+
+//TRIANGLES
+function makeDownwardStairs(height) {
+    let stairs = '';
+    for (let i = 0; i < height; i++) {
+        stairs += makeLine(height + 1) + '\n';
+    };
+    return stairs;
+  };
+//console.log(makeDownwardStairs(5));
