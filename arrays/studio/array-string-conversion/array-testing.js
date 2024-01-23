d1ef57dd8db4d3dae8bbbd309307c3d55627b185
Lorri Lanig <lorrilanig.art@gmail.com>
2024-01-22 18:12:16 -0600

    finished chapter9

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -4,13 +4,19 @@ let protoArray3 = "space delimited string";
 let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
-
+console.log(strings.includes(';',',',' '));
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
 	let check;
 	let output;
 	//TODO: 2. write the code required for this step
+	if (strings.includes(';',',',' ')) {
+		strings.split(',').join(' ');
+		console.log(strings);
+	}
+	console.log(reverseCommas);
+	
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
--- loops/chapter-examples/Reversing-a-String.js
+++ loops/chapter-examples/Reversing-a-String.js
@@ -6,3 +6,7 @@ for (let i = 0; i < str.length; i++) {
 }
 
 console.log(reversed);
+
+for (let i = ; i < str.length; i--) {
+    console.log(str[i]);
+}
