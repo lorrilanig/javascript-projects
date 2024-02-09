2d249807176319f9b3a89da2b8e80449282c4da6
Lorri Lanig <lorrilanig.art@gmail.com>
2024-02-08 20:20:21 -0600

    stuido-unit-testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,12 @@
 
 let launchcode = {
-  
+  organization : "nonprofit",
+  executiveDirector : "Jeff",
+  percentageCoolEmployees : 100,
+  programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+  method : launchOutput = str => {
+
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,9 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
-  // Write your unit tests here!
   
+  // Write your unit tests here!
+  test("should return 'nonprofit' as the organization", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+ });
 });
