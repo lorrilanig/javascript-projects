70209114f855985708fb848465f5a572964a027e
Lorri Lanig <lorrilanig.art@gmail.com>
2024-02-15 20:14:14 -0600

    studio-complete

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,6 +1,30 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
+class CrewCandidate {
+    constructor(name, mass, scores = []) {
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
+    }
+    addScore(num) {
+        this.scores.push(num)
+        return this.scores;
+        }
+    avearge() {
+        let sum = 0;
+        for (let i = 0; i < this.scores.length; i++) {
+           sum = sum + this.scores[i]
+        }
+        return Math.round(sum/this.scores.length); 
+        }
+    }
 
 
+
+
+let bBear = new CrewCandidate('Bubba Bear', 135, [88,85,90]);
+console.log(bBear);
+bBear.addScore(83);
+console.log(bBear.avearge());
 
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 
