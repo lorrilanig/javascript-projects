b817ba03654816f57453bf3eb457f3c3921d17e3
Lorri Lanig <lorrilanig.art@gmail.com>
2024-02-26 15:05:19 -0600

    this-is-EXCITING

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,9 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
     
 }
 
